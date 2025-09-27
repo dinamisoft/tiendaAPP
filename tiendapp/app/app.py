e444076f2089f15d04ff05d8c1660aa0651ffdec
juangserrano2 <128563212+juangserrano2@users.noreply.github.com>
2025-09-27 18:07:58 -0500

    Actualización de archivos config y app
    Se actualizan los archivos: config.py, app.py y bd.py

---
+++ tiendapp/app/app.py
@@ -0,0 +1,120 @@
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_wtf import CSRFProtect
+
+from config import config
+
+import db
+
+app = Flask(__name__)
+
+csfr = CSRFProtect(app)
+login_manager_app = LoginManager(app)
+
+#-- Entidades
+class Usuario(UserMixin):
+    def __init__(self, IdUsuario, PasswordUsuario="") -> None:
+        self.id = IdUsuario
+        self.password = PasswordUsuario
+        
+    @classmethod
+    def check_password(self, hashed_password, PasswordUsuario):
+        return check_password_hash(hashed_password, PasswordUsuario)
+ 
+#print(generate_password_hash("hola"))
+
+#-- Modelos
+class ModeloUsuario():
+     
+    @classmethod
+    def login(self, usuario) -> None:
+        try:
+            con = db.conexion()
+            cursor = con.cursor()
+            sql = """SELECT IdUsuario, PasswordUsuario 
+                     FROM tblusuarios WHERE IdUsuario = '{}'""".format(usuario.IdUsuario)
+            cursor.execute(sql)
+            row = cursor.fetchone()
+            if row != None:
+                usuario = Usuario(row[0], Usuario.check_password(row[1], usuario.PasswordUsuario))
+                return usuario
+            else:
+                return None
+        except Exception as ex:
+            raise RuntimeError(ex)
+    
+    @classmethod
+    def get_by_id(self, db, id) -> 'Usuario | None':
+        try:
+            con = db.conexion()
+            cursor = con.cursor()
+            sql = "SELECT IdUsuario, PasswordUsuario FROM tblUsuarios WHERE IdUsuario = {}".format(id)
+            cursor.execute(sql)
+            row = cursor.fetchone()
+            if row != None:
+                return Usuario(row[0], None, row[1])
+            else:
+                return None
+        except Exception as ex:
+            raise RuntimeError(ex)
+
+
+@login_manager_app.user_loader #Crear este método para loguear y confirmar el usuario
+def load_user(id):
+    return ModeloUsuario.get_by_id(db, id)
+
+            
+#-- Endpoints --
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        #print(request.form['username'])
+        #print(request.form['password'])
+        #return render_template('/auth/login.html')
+        usuario = Usuario(0,request.form['username'], request.form['password'])
+        logged_user = ModeloUsuario.login(usuario)
+        if logged_user != None:
+            if logged_user.password:
+                login_user(logged_user)
+                return redirect(url_for('cliente'))
+            else:
+                flash('Contraseña inválida')
+                return render_template('login.html')
+        else:
+            flash('Usuario no encontrado...')
+            return render_template('login.html')
+    else:
+        return render_template('login.html')
+    
+@app.route('/cliente')
+def cliente():
+    return redirect(url_for('cliente.html'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/protected')
+@login_required
+def protected():
+    return '<h1>Es una vista protegida, sólo para usuarios autenticados!</h1>'
+
+def status_401(error):
+    return redirect(url_for('login'))
+
+def status_404(error):
+    return '<h1>Página no encontrada</h1>', 404
+ 
+if __name__ == '__main__':
+    app.config.from_object(config['development'])
+    csfr.init_app(app)
+    app.register_error_handler(401, status_401)
+    app.register_error_handler(404, status_404)
+    app.run()
+ 
---
+++ tiendapp/app/db.py
@@ -0,0 +1,18 @@
+from flask import g
+import sqlite3
+
+#--- Conexión a SQLite ---
+def conexion():
+    try:
+        if 'conexion' not in g:
+            g.conexion = sqlite3.connect("..\\db\\tiendapp.db")
+        return g.conexion
+    except Exception:
+        return "No hay conexión con la base de datos"
+
+def cerrar_bd():
+    conexion = g.pop('conexion', None)
+    if conexion is not None:
+        conexion.close()
+
+#--- Fin conexión a SQLite ---
